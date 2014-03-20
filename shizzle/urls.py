1448721f0598a20ed57b3bd3c45def18c3c83fec
Doug Dodd <dougdodd@gmail.com>
2014-03-20 15:28:50 -0700

    tweaks, ending part 3 of Django tutorial

--- shizzle/urls.py
+++ shizzle/urls.py
@@ -1,10 +1,10 @@
 # This also imports the include function
-from django.conf.urls import *
+from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^polls/', include('polls.urls')), 
+                       url(r'^polls/', include('polls.urls', namespace='polls')), 
                        url(r'^admin/', include(admin.site.urls)),
 )
